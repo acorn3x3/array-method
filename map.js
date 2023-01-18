af9f0b453bd1b85a644fdb377393b79bc477d329
Andrew Cornejo <atcornejo3@gmail.com>
2023-01-18 09:49:54 -0800

    mapped countries and iso 2 test passes

--- map.js
+++ map.js
@@ -10,10 +10,13 @@ export const getCountryNames = (countries) => {
 };
 
 // INPUT: the array of countries from data.js
-// OUTPUT: an array of the names and their iso2 country codes using .map
+// OUTPUT: an array of the names and their iso2 (country)s codes using .map
 // REQS: use .map
 
-export const getCountryCodes = (countries) => {};
+export const getCountryCodes = (countries) => {
+  const countryCodes = countries.map((countries) => [countries.name, countries.iso2]);
+  return countryCodes;
+};
 
 // INPUT: the array of shoes from data.js
 // OUTPUT: an array of the brands
